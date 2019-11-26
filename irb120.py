0d7d538aab6a680a79b0b0aed8c1263e88f5e9be
AswinRetnakumar <aswint44@gmail.com>
2019-11-26 17:33:14 +0530

    Velocity computation added

--- irb120.py
+++ irb120.py
@@ -8,24 +8,44 @@ pb.setGravity(0,0,-9.8)
 planeId = pb.loadURDF("plane.urdf")
 bot = pb.loadURDF("irb120_3_58.urdf",[0, 0, 0], useFixedBase=1)
 
+def getJointStates(robot):
+  joint_states = pb.getJointStates(robot, range(pb.getNumJoints(robot)))
+  joint_positions = [state[0] for state in joint_states]
+  joint_velocities = [state[1] for state in joint_states]
+  joint_torques = [state[3] for state in joint_states]
+  return joint_positions, joint_velocities, joint_torques
+
 def main():
 
-    pb.setJointMotorControlArray(bot, range(6), pb.TORQUE_CONTROL,forces=[1600, 0.0 , 0 , 0, 0, 0])
-    for _ in range(20):
-        pb.stepSimulation()
-        time.sleep(0.5)
+        print(pb.getNumJoints(bot))
+        pb.setJointMotorControlArray(bot, range(pb.getNumJoints(bot)), pb.POSITION_CONTROL,targetPositions=[0.0, 0.0,0.0,0.0,0.0,0.0,0,0])
+        for _ in range(10000):
+                pb.stepSimulation()
+                time.sleep(0.1)
+                
 
 
-
-
-def joint_states():
-
-    joint_positions = [j[0] for j in pb.getJointStates(bot, range(6))]
-    print(joint_positions)
-
-def move(torques):
-    raise NotImplementedError
-
+def move(bot, torques):
+        prev = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        for i in range(0,5000,10):
+                #forces for torque cntrl and target_position s for pos control                                                        
+                #base =[-590, 590],shoulder, elbow,                            
+                pb.setJointMotorControlArray(bot, range(pb.getNumJoints(bot)), pb.TORQUE_CONTROL, forces= torques) # fourth state is unused
+                                                                                                # i dont know why
+                jp, jv, jt = getJointStates(bot)
+                '''if (jp[1])< -1.5:
+                        print(jp)
+                        print(i)
+                        pb.disconnect()
+                        break
+                '''
+                prev = jp
+                print("pos:", jp)
+                print("vel:", jv)
+                time.sleep(0.2)
+                pb.stepSimulation()
+        time.sleep(5)
+    
 if __name__ == '__main__':
-    main()
-    joint_states()
+    move(bot,[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+    
