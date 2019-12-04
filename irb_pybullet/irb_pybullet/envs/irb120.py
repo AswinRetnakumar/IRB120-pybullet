8f6bc5c0a35acd418458a6ca75e537b0ed2ac14e
AswinRetnakumar <aswint44@gmail.com>
2019-12-04 17:32:31 +0530

    AAAAA

--- irb_pybullet/irb_pybullet/envs/irb120.py
+++ irb_pybullet/irb_pybullet/envs/irb120.py
@@ -8,6 +8,9 @@ pb.setAdditionalSearchPath(pybullet_data.getDataPath())
 pb.setGravity(0,0,-9.8)
 planeId = pb.loadURDF("plane.urdf")
 bot = pb.loadURDF("irb120_3_58.urdf",[0, 0, 0], useFixedBase=1)
+#pb.setTimeStep(20)
+pb.setRealTimeSimulation(0)
+
 
 def getJointStates(robot):
   joint_states = pb.getJointStates(robot, range(pb.getNumJoints(robot)))
@@ -20,12 +23,13 @@ def getJointStates(robot):
 def main():
 
         print(pb.getNumJoints(bot))
-        pb.setJointMotorControlArray(bot, range(6), pb.POSITION_CONTROL,targetPositions=[0.0, 0.0,-2.7,0,0.0,1])
+        pb.setJointMotorControlArray(bot, range(6), pb.POSITION_CONTROL,targetPositions=[1.9, 0.0,0.0,0.0,0.0,0])
         #print(pb.getBasePositionAndOrientation(bot))
-        
+        #pb.stepSimulation()
         for _ in range(100):
                 pb.stepSimulation()
-                time.sleep(0.1)
+                #time.sleep(0.1)
+        time.sleep(2)
         pos,ore,_,_, _, _ = pb.getLinkState(bot, 6)
         pose = list(pos)+list(ore)
         print "Init pose: ", pose       
@@ -33,6 +37,6 @@ def main():
 if __name__ == '__main__':
         pos,ore,_,_, _, _ = pb.getLinkState(bot, 6)
         print "init pos and orientation", pos, " ", ore
-        print "joint limits: ", pb.getJointInfo(bot,3)[7:10]
+        print "joint limits: ", pb.getJointInfo(bot,6)[7:10]
         
         main()
--- irb_pybullet/irb_pybullet/envs/irb_openai.py
+++ irb_pybullet/irb_pybullet/envs/irb_openai.py
@@ -12,8 +12,9 @@ physicsClient = pb.connect(pb.GUI)
 pb.setAdditionalSearchPath(pybullet_data.getDataPath()) 
 pb.setGravity(0,0,-9.8)
 planeId = pb.loadURDF("plane.urdf")
+#pb.setTimeStep(0.0001)
 #bot = pb.loadURDF("irb120_3_58.urdf",[0, 0, 0], useFixedBase=1)
-
+pb.setRealTimeSimulation(0)
 
 class OpenaiIRB(gym.core.Env):
     
@@ -30,15 +31,15 @@ class OpenaiIRB(gym.core.Env):
         """Observation space consists of pose data"""
         self.init_q = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self.init_pose = [0.374, -2.439454888092385e-19, 0.6299999999999999, 0.0, 0.0, 0.0, 1.0 ]
-	self.init_vel = [0.0]*6
+        self.init_vel = [0.0]*6
         self._goal_pose = [0.2665122782069116, 0.40988654527750035, 0.16178715865384272 , 0.1685172244060629, 0.568363626537449, 0.16851711653306128, 0.7875066441262947]
         self.observation_space = gym.spaces.Box(
             low=np.array([-np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf]),
             high=np.array([np.inf, np.inf, np.inf, np.inf, np.inf, np.inf]))
         
         self.action_space = gym.spaces.Box(
-            low  = np.array([-1.91, -1.91, -2.5, -np.pi/2, -5*np.pi/8, -2*np.pi]), 
-            high = np.array([1.91, 0.9, 0.8, np.pi/2, 5*np.pi/8, 2*np.pi]))
+            low  = np.array([-1.91, -1.91, -2.5, -2.0, -1.5, -1]), 
+            high = np.array([1.91, 0.9, 0.8, 2.0, 1.5, 0.0]))
 
         self.bot = pb.loadURDF("irb120_3_58.urdf",[0, 0, 0], useFixedBase=1)
 
@@ -87,7 +88,8 @@ class OpenaiIRB(gym.core.Env):
         # If yes done is set to true
         print "step function"
         pb.setJointMotorControlArray(self.bot, range(6), pb.POSITION_CONTROL,targetPositions= action)
-        pb.stepSimulation()
+        for _ in range(100):
+                pb.stepSimulation()
         pos,ore,_,_, _, _ = pb.getLinkState(self.bot, 6)
         state = list(pos)+list(ore)
         reward,done = self.reward_compute(state)
@@ -97,3 +99,4 @@ class OpenaiIRB(gym.core.Env):
           
     def render(self, mode='human', close=False):
         print("Dont know what to do....... ")
+
