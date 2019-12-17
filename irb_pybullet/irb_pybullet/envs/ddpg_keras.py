285d2f1e146bfdd812bb4fa9e6ca858e6214440f
AswinRetnakumar <aswint44@gmail.com>
2019-12-17 13:57:50 +0530

    ddpg updated with adabatch

--- irb_pybullet/irb_pybullet/envs/ddpg_keras.py
+++ irb_pybullet/irb_pybullet/envs/ddpg_keras.py
@@ -29,14 +29,13 @@ def Create_Net(s, action, layer_size, num_layers, action_size):
     with tf.variable_scope('q'):
         input_ = tf.concat([s, action], axis=-1) # (state, action)
         q = tf.squeeze(Network(input_, layer_size+[1]), axis=1)
+        
     with tf.variable_scope('q', reuse=True):
         # reuse is True, so it reuses the weights from the previously defined Q network
         input_ = tf.concat([s, mu], axis=-1) # (state, network actions)
         q_mu = tf.squeeze(Network(input_, layer_size+[1]), axis=1)
     
     return mu, q, q_mu
-
-
 
 
 
